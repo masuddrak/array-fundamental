20487c7489d8e779f64462de21697564e88f7730
masud rana <masudranaabc@gmail.com>
2023-03-30 21:59:15 +0600

    string revers

---
+++ revers_string.js
@@ -0,0 +1,8 @@
+let name='i love You ';
+let revers_name=''
+for(let i=name.length-1; i>=0; i--){
+    revers_name=revers_name+name[i]
+}
+console.log(name)
+console.log('------------------')
+console.log(revers_name)
